2018409ab6a17a8d2f1ca44a3cc947f9b0761cb8
owenmagri24 <magriowen2410@gmail.com>
2021-12-02 11:22:37 +0100

    Started working on Plant Growing
    --Created PlantTesting Scene
    --Created PlantPot Script
    --When box collides with Plant Pot, top collider moves toward target height, creating a lift style

---
+++ Assets/Scripts/PlantScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantScript : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject m_TopCollision;
+    private Vector3 m_TargetPosition;
+
+    [SerializeField]
+    private float m_LiftSpeed;
+
+    [SerializeField]
+    private float m_LiftHeight;
+
+    void Start()
+    {
+        m_TargetPosition = m_TopCollision.transform.position;
+
+    }
+
+    void FixedUpdate()
+    {
+        if(m_TopCollision.transform.position.y < m_TargetPosition.y )
+        {
+            m_TopCollision.transform.Translate(Vector3.up * m_LiftSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.name == "Box")
+        {
+            m_TargetPosition = new Vector3(m_TargetPosition.x, m_LiftHeight, m_TargetPosition.z);
+        }
+    }
+}
