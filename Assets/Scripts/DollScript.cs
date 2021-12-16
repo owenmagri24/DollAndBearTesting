218d58e83f9cb04fade6bdf867f1a286dfd0b921
owenmagri24 <magriowen2410@gmail.com>
2021-12-16 10:31:14 +0100

    Bug Fixes
    --Fixed Bug where box keeps moving after picking up
    --Changed box Layer so player can jump on it

--- Assets/Scripts/DollScript.cs
+++ Assets/Scripts/DollScript.cs
@@ -9,28 +9,26 @@ public class DollScript : CharacterBase
     public override void OnInteract(){
         base.OnInteract();
 
-        if(m_hit.collider != null){
-            m_ObjectHit = m_hit.collider.gameObject;
 
-            if(m_ObjectHit.tag == "PushableObject" && m_HoldingObject == false)
-            {
-                //Picking up Pushable Objects
+        if(m_ObjectHit.tag == "PushableObject" && m_HoldingObject == false)
+        {
+            //Picking up Pushable Objects
 
-                Physics2D.IgnoreCollision(m_hit.collider, m_BoxCollider2D); //ignores picked up object collider
-                m_HoldingObject = true;
-                m_ObjectHit.transform.parent = m_BoxHolder;
-                m_ObjectHit.transform.position = m_BoxHolder.position;
-                m_ObjectHit.GetComponent<Rigidbody2D>().isKinematic = true;
-            }
-            else if(m_HoldingObject == true)
-            {
-                //Releasing Pushable Objects
+            Physics2D.IgnoreCollision(m_hit.collider, m_BoxCollider2D); //ignores picked up object collider
+            m_HoldingObject = true;
+            m_ObjectHit.transform.parent = m_BoxHolder;
+            m_ObjectHit.transform.position = m_BoxHolder.position;
+            m_ObjectHit.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            m_ObjectHit.GetComponent<Rigidbody2D>().isKinematic = true;
+        }
+        else if(m_HoldingObject == true)
+        {
+            //Releasing Pushable Objects
 
-                Physics2D.IgnoreCollision(m_hit.collider, m_BoxCollider2D, false);//enables collision on release
-                m_HoldingObject = false;
-                m_ObjectHit.transform.parent = null;
-                m_ObjectHit.GetComponent<Rigidbody2D>().isKinematic = false;
-            }
+            Physics2D.IgnoreCollision(m_hit.collider, m_BoxCollider2D, false);//enables collision on release
+            m_HoldingObject = false;
+            m_ObjectHit.transform.parent = null;
+            m_ObjectHit.GetComponent<Rigidbody2D>().isKinematic = false;
         }
     }
 }
--- Assets/Scripts/GirlScript.cs
+++ Assets/Scripts/GirlScript.cs
@@ -8,13 +8,10 @@ public class GirlScript : CharacterBase
     public override void OnInteract()
     {
         base.OnInteract();
-
-        if(m_hit.collider != null){
-            m_ObjectHit = m_hit.collider.gameObject;
-
-            if(m_ObjectHit.tag == "Bear"){
-                m_ObjectHit.SetActive(false);
-            }
+        
+        if(m_ObjectHit.tag == "Bear"){
+            m_ObjectHit.SetActive(false);
         }
+        
     }
 }
