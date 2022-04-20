2f4b81c58fe92b990400e5aa2d58d84e11ce08b6
owenmagri24 <magriowen2410@gmail.com>
2022-04-20 14:07:17 +0200

    Created Slope Movement
    -- Created slope movement for girl and bear in CharachterBase script
    -- Created physics materials for full friction and no friction so character doesn't slide down on slope
    -- Changed gravity and jump force value fo girl and bear to make them feel less 'floaty'

--- Assets/Scripts/Base/CharacterBase.cs
+++ Assets/Scripts/Base/CharacterBase.cs
@@ -14,13 +14,36 @@ public abstract class CharacterBase : MonoBehaviour
 
     protected Rigidbody2D m_Rigidbody2D;
 
-    protected Collider2D m_BoxCollider2D;
+    protected CapsuleCollider2D m_Capsulecollider2D;
+
+    protected Vector2 m_CapsuleSize;
+
+    protected Vector2 m_SlopeNormalPerp;
+
+    protected float m_SlopeDownAngle;
+
+    protected bool m_IsOnSlope;
+
+    protected float m_SlopeDownAngleOld;
+
+    protected float m_SlopedSideAngle;
+
+    protected bool m_IsJumping;
+
+    [SerializeField]
+    protected PhysicsMaterial2D m_NoFriction;
+
+    [SerializeField]
+    protected PhysicsMaterial2D m_FullFriction;
+
+    [SerializeField]
+    protected float slopeCheckDistance;
 
     [SerializeField]
     protected float m_JumpForce = 500f;
 
     [SerializeField]
-    protected float m_JumpCheckToFloor = 1.01f;
+    protected float m_JumpCheckToFloor;
 
     [SerializeField]
     protected float m_Speed = 5f;
@@ -48,8 +71,9 @@ public abstract class CharacterBase : MonoBehaviour
 
     protected virtual void Awake()
     {
+        m_Capsulecollider2D = GetComponent<CapsuleCollider2D>();
+        m_CapsuleSize = m_Capsulecollider2D.size;
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
-        m_BoxCollider2D = GetComponent<Collider2D>();
         m_RespawnPointManager = FindObjectOfType<RespawnPointManager>();
         m_BallControl = FindObjectOfType<BallControl>();
         m_UIManager = FindObjectOfType<UIManager>();
@@ -78,10 +102,34 @@ public abstract class CharacterBase : MonoBehaviour
     }
 
     protected virtual void FixedUpdate() {
-        m_Rigidbody2D.velocity = new Vector2(direction.x * m_Speed, m_Rigidbody2D.velocity.y);
+        //m_Rigidbody2D.velocity = new Vector2(direction.x * m_Speed, m_Rigidbody2D.velocity.y);
         Physics2D.queriesStartInColliders = false; //Avoids ray collisions from hitting own object
 
+        SlopeCheck();
+        ApplyMovement();
+
         animator.SetFloat("Vertical Velocity", m_Rigidbody2D.velocity.y);
+    }
+
+    protected virtual void ApplyMovement()
+    {
+        m_Rigidbody2D.velocity = new Vector2(direction.x * m_Speed, m_Rigidbody2D.velocity.y);
+
+        if(IsGrounded() && !m_IsOnSlope && !m_IsJumping)
+        {
+            Debug.Log("Grounded and not on slope");
+            m_Rigidbody2D.velocity = new Vector2(direction.x * m_Speed, 0.0f);
+        }
+        else if(IsGrounded() && m_IsOnSlope && !m_IsJumping )
+        {
+            Debug.Log("Grounded and on slope");
+            m_Rigidbody2D.velocity = new Vector2(m_SlopeNormalPerp.x * m_Speed *  -direction.x, m_SlopeNormalPerp.y * m_Speed *  -direction.x);
+        }
+        else if(!IsGrounded())
+        {
+            Debug.Log("not grounded");
+            m_Rigidbody2D.velocity = new Vector2(direction.x * m_Speed, m_Rigidbody2D.velocity.y);
+        }
     }
 
     public void OnMovement(InputValue value)
@@ -100,15 +148,20 @@ public abstract class CharacterBase : MonoBehaviour
 
     public void OnJump() {
         if (IsGrounded()) {
+            m_IsJumping = true;
             m_AudioManager.Play("PlayerJump");
             m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce), ForceMode2D.Impulse);
-            
         }
     }
 
     protected bool IsGrounded() {
         int layers = 1 << LayerMask.NameToLayer("Platforms") | 1 << LayerMask.NameToLayer("InteractableObject");
         RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, Vector2.down, m_JumpCheckToFloor, layers);
+
+        if(m_Rigidbody2D.velocity.y <= 0.0f)
+        {
+            m_IsJumping = false;
+        }
         return raycastHit2D.collider != null;
     }
 
@@ -138,15 +191,16 @@ public abstract class CharacterBase : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.transform.IsChildOf(m_CushionsList.transform) && (other.gameObject.name == ("CushionBigBounce")))
         {
+            m_IsJumping = true;
             m_Rigidbody2D.AddForce(Vector2.up * m_JumpForce * 2.5f, ForceMode2D.Impulse);
             m_AudioManager.Play("PillowJump");
         }
         else if(other.gameObject.transform.IsChildOf(m_CushionsList.transform))
         {
+            m_IsJumping = true;
             m_Rigidbody2D.AddForce(Vector2.up * m_JumpForce * 1.5f, ForceMode2D.Impulse);
             m_AudioManager.Play("PillowJump");
         }
-        
     }
 
 
@@ -180,6 +234,70 @@ public abstract class CharacterBase : MonoBehaviour
         {
             Debug.Log("exited");
             transform.parent = null;
+        }
+    }
+
+    protected virtual void SlopeCheck()
+    {
+        Vector2 checkPos = transform.position - new Vector3(0.0f, m_CapsuleSize.y /2);
+
+        SlopeCheckHorizontal(checkPos);
+        SlopeCheckVertical(checkPos);
+    }
+
+    protected virtual void SlopeCheckHorizontal(Vector2 checkPos)
+    {
+        int layers = 1 << LayerMask.NameToLayer("Platforms") | 1 << LayerMask.NameToLayer("InteractableObject");
+        RaycastHit2D slopeHitFront = Physics2D.Raycast(checkPos, transform.right, slopeCheckDistance, layers);
+        RaycastHit2D slopeHitBack = Physics2D.Raycast(checkPos, -transform.right, slopeCheckDistance, layers);
+
+        if(slopeHitFront)
+        {
+            m_IsOnSlope = true;
+            m_SlopedSideAngle = Vector2.Angle(slopeHitFront.normal, Vector2.up);
+        }
+        else if(slopeHitBack)
+        {
+            m_IsOnSlope = true;
+            m_SlopedSideAngle = Vector2.Angle(slopeHitBack.normal, Vector2.up);
+        }
+        else
+        {
+            m_SlopedSideAngle = 0.0f;
+            m_IsOnSlope = false;
+            m_JumpCheckToFloor = 1.01f;
+        }
+    }
+
+    protected virtual void SlopeCheckVertical(Vector2 checkPos)
+    {
+        int layers = 1 << LayerMask.NameToLayer("Platforms") | 1 << LayerMask.NameToLayer("InteractableObject");
+
+        RaycastHit2D hit = Physics2D.Raycast(checkPos, Vector2.down, slopeCheckDistance, layers);
+        if(hit)
+        {
+            m_SlopeNormalPerp = Vector2.Perpendicular(hit.normal).normalized;
+
+            m_SlopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            if(m_SlopeDownAngle != m_SlopeDownAngleOld)
+            {
+                m_IsOnSlope = true;
+                m_JumpCheckToFloor = 1.25f;
+            }
+            m_SlopeDownAngleOld = m_SlopeDownAngle;
+
+            Debug.DrawRay(hit.point, m_SlopeNormalPerp, Color.red);
+            Debug.DrawRay(hit.point, hit.normal, Color.green);
+        }
+
+        if(m_IsOnSlope && direction.x == 0.0f)
+        {
+            m_Rigidbody2D.sharedMaterial = m_FullFriction;
+        }
+        else
+        {
+            m_Rigidbody2D.sharedMaterial = m_NoFriction;
         }
     }
 }
